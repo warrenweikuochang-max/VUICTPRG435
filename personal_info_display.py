135d311df38c14109868280eeb7cec91a614dd47
warrenweikuochang-max <warren.weikuochang@gmail.com>
2025-11-09 16:38:31 +1100

    modify personal file

--- personal_info_display.py
+++ personal_info_display.py
@@ -10,3 +10,35 @@ print(is_student)
 
 
 # Python Testing Wazza
+
+types_of_people = 10
+types_of_people = 10
+
+x = f"They are yumma {types_of_people} types of people"
+
+binary = "binary"
+
+do_not = "don't"
+  
+y = f"aiyoo those who know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+
+print(f"i said: {x}")
+
+print(f"I also said: '{y}'")
+
+hilarious = False
+
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious)) #what is this lecturer trying to do to us
+
+w = "This is the left side of..."
+
+e = "a string with a right side."
+
+print(w + e)
+
